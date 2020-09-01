ed830d7cacce66b296489f4a46a7d24621128838
Lim Li Khia <likhia@hotmail.com>
2020-09-01 23:49:28 +0800

    use bean 5

--- src/main/java/org/mycompany/bean/QuickConfiguration.java
+++ src/main/java/org/mycompany/bean/QuickConfiguration.java
@@ -1,6 +1,5 @@
 package org.mycompany.bean;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
