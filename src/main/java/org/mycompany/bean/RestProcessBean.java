33a1933ff174eb27c7df1328199a01ad88f2a316
Lim Li Khia <likhia@hotmail.com>
2020-09-01 23:00:11 +0800

    use bean

---
+++ src/main/java/org/mycompany/bean/RestProcessBean.java
@@ -0,0 +1,24 @@
+package org.mycompany.bean;
+
+import org.apache.camel.Exchange;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RestProcessBean {
+	
+	@Autowired
+	QuickConfiguration config;
+
+	public OutputBean process(Exchange exchange) throws Exception {
+    	
+    	OutputBean out = new OutputBean();
+    	
+        //Get input from exchange
+    	InputBean inputParam = (InputBean) exchange.getIn().getBody(InputBean.class);
+        
+        out.setResult(config.getPrefix() + " " + inputParam.getField1() + " " + inputParam.getField2() );
+        
+        
+       return out;
+    }
+
+}
